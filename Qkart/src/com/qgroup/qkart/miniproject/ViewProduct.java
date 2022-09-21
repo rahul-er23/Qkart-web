201781214a954b0dabc93f01852f426b504d6ded
amolmahajan <amolmahajan1998@gmail.com>
2022-09-21 13:36:54 +0530

    try block changes

--- Qkart/src/com/qgroup/qkart/miniproject/ViewProduct.java
+++ Qkart/src/com/qgroup/qkart/miniproject/ViewProduct.java
@@ -36,9 +36,11 @@ public class ViewProduct extends LoginRegister {
 		try {
 			pstate = con.prepareStatement("select * from productdetails where ProductID=" + id);
 			pro = pstate.executeQuery();
+			System.out.printf("%-12s%-30s%-35s%-18s%-15s\n\n","User ID","Product Name","Discription","Price","Available Quantity");
 			while (pro.next()) {
-				System.out.println("\nProduct ID: " + pro.getInt(1) + "\nName: " + pro.getString(3) + "\nDescription: "
-						+ pro.getString(2) + "\nPrice: " + pro.getString(4) + "\nAvailable Quantity: " + pro.getInt(5));
+			System.out.printf("%-12s%-30.30s%-35.30s%-18s%-15s\n",pro.getInt(1),pro.getString(3),pro.getString(2),pro.getString(4),pro.getString(5));
+				//System.out.println("\nProduct ID: " + pro.getInt(1) + "\nName: " + pro.getString(3) + "\nDescription: "
+					//	+ pro.getString(2) + "\nPrice: " + pro.getString(4) + "\nAvailable Quantity: " + pro.getInt(5));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
