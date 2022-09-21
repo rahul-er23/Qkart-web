c2924a5aa4ef2af19d7f8074e79758efa90929bf
amolmahajan <amolmahajan1998@gmail.com>
2022-09-21 10:46:10 +0530

    ViewProduct extends LoginRegister

--- Qkart/src/com/qgroup/qkart/miniproject/ViewProduct.java
+++ Qkart/src/com/qgroup/qkart/miniproject/ViewProduct.java
@@ -1,5 +1,8 @@
 package com.qgroup.qkart.miniproject;
 
-public class ViewProduct {
+public class ViewProduct extends LoginRegister {
 
-}
+	public void allProduct(int id) {
+		System.out.println("\nAll Product");
+
+	}}
